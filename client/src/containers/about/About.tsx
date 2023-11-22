075ca9afda82c6f05a59360c365f03f19dfecacf
Cyrus Chan <sairuscsc@gmail.com>
2023-11-22 18:37:00 -0500

    add images to header and about

--- client/src/containers/about/About.tsx
+++ client/src/containers/about/About.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { client } from '../../../utils/sanity';
+import { client, urlFor } from '../../../utils/sanity';
 import { aboutQuery } from '../../../utils/queries';
 import { About } from '../../../types/sanity';
 import { PortableText } from '@portabletext/react';
@@ -15,18 +15,32 @@ const About = () => {
 
   return (
     <section id='about' className='app__container app__about'>
-      <motion.div
-        whileInView={{ y: [100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-      >
-        <h2 className='subtitle-text'>{about.title}</h2>
-      </motion.div>
-      <motion.div
-        whileInView={{ y: [100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5, delay: 0.5 }}
-      >
-        <PortableText value={about.description} />
-      </motion.div>
+      <div className='app__about-container'>
+        <div className='text-container'>
+          <motion.div
+            whileInView={{ y: [100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+          >
+            <h2 className='subtitle-text'>{about.title}</h2>
+          </motion.div>
+          <motion.div
+            whileInView={{ y: [100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+          >
+            <PortableText value={about.description} />
+          </motion.div>
+        </div>
+        <div className='image-container'>
+          {about.image && (
+            <motion.img
+              src={urlFor(about.image).url()}
+              alt={about.title}
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5, delay: 1 }}
+            />
+          )}
+        </div>
+      </div>
     </section>
   );
 };
--- client/src/containers/header/Header.tsx
+++ client/src/containers/header/Header.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import './Header.scss';
 import { useEffect, useState } from 'react';
-import { client } from '../../../utils/sanity';
+import { client, urlFor } from '../../../utils/sanity';
 import { brandQuery } from '../../../utils/queries';
 import { Brand } from '../../../types/sanity';
 
@@ -14,25 +14,39 @@ const Header = () => {
 
   return (
     <section className='app__container app__header'>
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-      >
-        <p className='p-text'>Hi, my name is</p>
-        <h1 className='title-text'>{brand.name}</h1>
-      </motion.div>
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5, delay: 0.5 }}
-      >
-        <h2 className='subtitle-text'>{brand.headline}</h2>
-      </motion.div>
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5, delay: 1 }}
-      >
-        <p className='p-text'>{brand.description}</p>
-      </motion.div>
+      <div className='app__header-container'>
+        <div className='text-container'>
+          <motion.div
+            whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5 }}
+          >
+            <p className='p-text'>Hi, my name is</p>
+            <h1 className='title-text'>{brand.name}</h1>
+          </motion.div>
+          <motion.div
+            whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5, delay: 0.5 }}
+          >
+            <h2 className='subtitle-text'>{brand.headline}</h2>
+          </motion.div>
+          <motion.div
+            whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+            transition={{ duration: 0.5, delay: 1 }}
+          >
+            <p className='p-text'>{brand.description}</p>
+          </motion.div>
+        </div>
+        <div className='image-container'>
+          {brand.image && (
+            <motion.img
+              src={urlFor(brand.image).url()}
+              alt={brand.name}
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5, delay: 1.5 }}
+            />
+          )}
+        </div>
+      </div>
     </section>
   );
 };
